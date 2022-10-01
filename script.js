a28286dab536e626b35034f09ecf635a07f4bc9f
Alex Guenot <aguenot75@gmail.com>
2022-10-01 13:56:18 +0200

    Missing how to make it work

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const resultDisplay  = document.querySelector('#resultDisplay')
+const digitsDisplay = document.getElementById("digits");
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) =>{
+    button.addEventListener('click', ()=> {
+      resultDisplay.innerHTML = button.innerHTML;
+    })
+})
 function addition(theArray) {
     return theArray.reduce((total,item) => total+item,0);
 }
@@ -17,14 +25,20 @@ function operate(operator, firstNumber, secondNumber) {
     return result
 }
 /* Displayer for the digits 1 to 9 */
-const digitsDisplay = document.getElementById("digits");
 function digitsDisplayer() {
   digitsDisplay.style.setProperty('--grid-rows', 3);
   digitsDisplay.style.setProperty('--grid-cols', 3);
   for (c = 0; c < 9; c++) {
     let cell = document.createElement("button");
     cell.innerText = (9-c);
+    /* The Digit is shown on the display result container  */
+    cell.addEventListener('click', ()=> {
+      resultDisplay.innerHTML = cell.innerText;
+    })
     digitsDisplay.appendChild(cell).className = "grid-item";
   };
 };
 digitsDisplayer()
+
+
+
